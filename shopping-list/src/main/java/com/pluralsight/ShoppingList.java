ef65ca954d781087f64ee8a36581e4c079dc4997
Abraham Belayhun <abrahambelayhun@gmail.com>
2025-04-10 11:28:53 -0500

    first java projrct

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("pool");
+        System.out.println("moon");
+        System.out.println("star");
+        System.out.println("air");
+        System.out.println("fire");
+        System.out.println("earth");
+        System.out.println("water");
+        System.out.println("sun");
+        System.out.println("double");
+        System.out.println("body");
+    }
+}
